9b0762a053c9492ad995fd6486f25b3450f388f3
hugendobler <luisach2003@gmail.com>
2023-09-12 20:35:25 -0300

    pronto

--- App.java
+++ App.java
@@ -1,5 +1,22 @@
-import util.Java.Scanner;
+import java.util.Scanner;
 
 public class App {
+
+    public static void main (String args[]){
+
+    Scanner scanner = new Scanner (System.in);
+    Contador contador = new Contador(500);
+
+    double valorIncremento;
+
+    System.out.printf("Saldo atual: %.2f\n", contador.getValorCount());
+
+    System.out.println("Valor a ser incrementado: ");
+    valorIncremento = scanner.nextDouble();
+    System.out.printf("Saldo atual: %.2f\n", contador.incrementar(contador.getValorCount(), valorIncremento));
+
+    System.out.printf("Saldo atual: %.2f\n", contador.zerar(contador.getValorCount()));
+
     
+    }
 }
